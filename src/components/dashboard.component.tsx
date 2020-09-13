03c9aeddd82e2ab6588f4e3406e6efbce83b8787
Ravinesh Sharma <sharma.ravinesh@gmail.com>
2020-09-14 00:24:11 +0530

    Removed unused imports

--- src/components/dashboard.component.tsx
+++ src/components/dashboard.component.tsx
@@ -1,15 +1,7 @@
-import { AppBar, Button, CssBaseline, Toolbar, Typography } from '@material-ui/core';
 import React, { Component } from 'react';
 import Header from '../functions/header';
 
 class Dashboard extends Component<any, any>{
-    constructor(probs: any) {
-        super(probs);
-    }
-
-    logout() {
-        this.props.history.push("");
-    }
 
     render() {
         return (
