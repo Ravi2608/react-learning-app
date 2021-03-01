b68a061e9fd3ad9e1d502f3c3f69d43ccd0cd524
Ravinesh Sharma <sharma.ravinesh@gmail.com>
2021-03-01 11:54:26 +0530

    Updated versions of dependencies

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard.component';
 import LoginComponent from './components/login.component';
 
